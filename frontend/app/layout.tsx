71bb221631e82ff226e8765a3a408cc55ddbaa65
arksomething <ark296296@gmail.com>
2025-11-05 21:23:41 -0500

    added milestone 2 md

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -7,7 +7,7 @@ const defaultUrl = process.env.NEXT_PUBLIC_SITE_URL
   ? process.env.NEXT_PUBLIC_SITE_URL
   : process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000"; // Fallback for local dev (won't be used in production)
+  : "http://localhost:3000";
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
